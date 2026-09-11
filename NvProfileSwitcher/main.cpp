9f0c5c9f64c8e30a63842c1c278a6b31fbfe3e85
Maximiliano Carnevali <38984659+mgcarnevali@users.noreply.github.com>
2026-09-11 12:21:44 -0300

    Fix dialog height type mismatch

--- NvProfileSwitcher/main.cpp
+++ NvProfileSwitcher/main.cpp
@@ -1033,7 +1033,7 @@ HWND CreateAppMessageWindow(AppMessageData* data,HWND owner){
         DT_CALCRECT|DT_WORDBREAK|DT_NOPREFIX);
     SelectObject(measureDc,oldFont);
     ReleaseDC(nullptr,measureDc);
-    const int windowHeight=std::max(228,measure.bottom+139);
+    const int windowHeight=std::max(228,static_cast<int>(measure.bottom)+139);
 
     HWND dialog=CreateWindowExW(WS_EX_DLGMODALFRAME|WS_EX_TOPMOST,
         L"NvProfileSwitcherMessage",data->title.c_str(),WS_CAPTION|WS_SYSMENU,
